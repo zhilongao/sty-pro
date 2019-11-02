b547cae44c1d4439662311768b008eb5b83c6d19
gzl <303547207@qq.com>
2019-11-02 10:14:09 +0800

    'httputil'

---
+++ common-util/src/main/java/com/util/dto/LocationDTO.java
@@ -0,0 +1,26 @@
+package com.util.dto;
+
+/**
+ * @Author long
+ * @Date 2019/11/2 9:41
+ */
+public class LocationDTO {
+    private double lat;
+    private double lng;
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+}
